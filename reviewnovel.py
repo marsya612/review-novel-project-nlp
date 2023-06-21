b2d3d23b8adfec8a2d4b066362efec92fcd42c12
Marsyanda <97005301+marsya612@users.noreply.github.com>
2023-06-21 21:30:51 +0700

    Update reviewnovel.py

--- reviewnovel.py
+++ reviewnovel.py
@@ -1,14 +1,11 @@
 import subprocess
 import sys
 
-# Fungsi untuk menginstal dependensi menggunakan pip
 def install_package(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-# List library yang ingin diinstal
 packages = ['streamlit', 'pandas', 'scikit-learn', 'nltk']
 
-# Menginstal library
 for package in packages:
     try:
         __import__(package)
@@ -16,7 +13,6 @@ for package in packages:
         print(f"Library {package} tidak ditemukan. Menginstal...")
         install_package(package)
 
-# Setelah instalasi selesai, import library yang dibutuhkan
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
@@ -31,7 +27,6 @@ from nltk.stem import WordNetLemmatizer
 import re
 import streamlit as st
 
-# Fungsi untuk membersihkan teks dari karakter khusus, mengubah menjadi huruf kecil, dan melakukan lemmatisasi
 def preprocess_text(text):
     text = re.sub('[^A-Za-z]', ' ', text)
     text = text.lower()
@@ -46,7 +41,6 @@ def preprocess_text(text):
 def sentiment_analysis(review):
     preprocessed_review = preprocess_text(review)
     vectorized_review = vectorizer.transform([preprocessed_review])
-    #prediction = classifier.predict(vectorized_review)
     score = classifier.decision_function(vectorized_review)[0]
     if score > 0.2:
         sentiment = 'Positive'
@@ -56,21 +50,17 @@ def sentiment_analysis(review):
         sentiment = 'Neutral'
     return sentiment, score
 
-# Baca dataset review novel
-dataset = pd.read_csv('reviewnovel.csv')  # Ganti 'dataset.csv' dengan nama file dataset Anda
+dataset = pd.read_csv('reviewnovel.csv')  
 reviews = dataset['Review'].tolist()
 
-# Preprocessing dan vektorisasi teks
 preprocessed_reviews = [preprocess_text(review) for review in reviews]
 vectorizer = TfidfVectorizer()
 vectorized_reviews = vectorizer.fit_transform(preprocessed_reviews)
 
-# Pelatihan model SVM
 labels = dataset['Rating'].apply(lambda x: 'positive' if x > 3 else 'negative')
 classifier = SVC(kernel='linear')
 classifier.fit(vectorized_reviews, labels)
 
-# Deploy model menggunakan Streamlit
 def main():
     st.title("Sentiment Analysis pada Review Novel")
     review = st.text_input("Masukkan review novel")
